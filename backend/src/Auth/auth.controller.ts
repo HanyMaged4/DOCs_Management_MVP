c4f94d0e2c616e73a5cf90dbb0d29050ec0c888f
HanyMaged4 <hany.maged9102@gmail.com>
2025-08-17 13:35:33 +0300

    add Signup DTo

--- backend/src/Auth/auth.controller.ts
+++ backend/src/Auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Post, Body } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { SignInDto } from "./DTOs/index";
 
 @Controller('auth')
 export class AuthController{
     constructor(private authService:AuthService){}
     @Post('signin')
-    signIn() {
+    signIn(@Body() SignInDTO: SignInDto) {
+        
         return this.authService.signIn();
     }
     @Post('signup')
--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,15 +1,23 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma';
 
 @Injectable()
-export class PrismaService extends PrismaClient{
+export class PrismaService extends PrismaClient implements OnModuleInit {
     constructor() {
         super({
-            datasources:{
+            datasources: {
                 db: {
                     url: process.env.DATABASE_URL,
                 },
             }, 
         });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
     }
 }
