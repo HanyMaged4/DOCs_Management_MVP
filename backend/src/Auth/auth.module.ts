e17b06f94476d2b8a88ab3d96730599e854bff7c
HanyMaged4 <hany.maged9102@gmail.com>
2025-08-18 14:38:59 +0300

    finished the tutorial and start coding

--- backend/src/Auth/auth.module.ts
+++ backend/src/Auth/auth.module.ts
@@ -2,13 +2,18 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { JwtModule } from "@nestjs/jwt";
-
+import { PassportModule } from "@nestjs/passport";
+import { JwtStrategy } from "./strategy/jwt.strategy";
 
 @Module({
-    imports:[JwtModule.register({})],
+    imports:[
+        PassportModule,
+        JwtModule.register({}) ,
+    ],
     controllers: [AuthController],
-    providers:[AuthService]
+    providers:[
+        AuthService,
+        JwtStrategy
+    ]
 })
-export class AuthModule {
-    
-}
+export class AuthModule {}
---
+++ backend/src/Auth/decorators/get-user.decorator.ts
@@ -0,0 +1,13 @@
+
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const User = createParamDecorator(
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.user;
+    if(data) {
+        return user ? user[data] : null; 
+    }
+    return data ? user?.[data] : user;
+  },
+);
