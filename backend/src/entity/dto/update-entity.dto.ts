ecf055d562d6f5ca7e78cf43a5dbfcd15134efc4
HanyMaged4 <hany.maged9102@gmail.com>
2025-08-19 17:26:43 +0300

    entities CRUD

--- backend/src/entity/dto/update-entity.dto.ts
+++ backend/src/entity/dto/update-entity.dto.ts
@@ -1,4 +1,20 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEntityDto } from './create-entity.dto';
+import { IsNotEmpty, IsNumber, IsOptional, IsString, Length } from "class-validator";
 
-export class UpdateEntityDto extends PartialType(CreateEntityDto) {}
+export class UpdateEntityDto {
+    @IsOptional()
+    @IsNumber()
+    book_id: number;
+
+    @IsOptional()
+    @IsString({ message: 'Tag title must be a string' })
+    @Length(1, 50, { message: 'Tag title must be between 1 and 50 characters' })
+    title: string;
+    
+    @IsOptional()
+    @Length(0, 5000, { message: 'Content must be at most 5000 characters' })
+    content: string; 
+
+    @IsOptional()
+    @IsNumber({}, { each: true, message: 'Tags must be an array of numbers' })
+    tags: string[];
+}
--- backend/src/entity/entity.controller.ts
+++ backend/src/entity/entity.controller.ts
@@ -1,34 +1,37 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { EntityService } from './entity.service';
 import { CreateEntityDto } from './dto/create-entity.dto';
 import { UpdateEntityDto } from './dto/update-entity.dto';
+import { JWTGuard } from 'src/Auth/guards';
+import { GetUser } from 'src/Auth/decorators/get-user.decorator';
 
-@Controller('entity')
+@UseGuards(JWTGuard)
+@Controller('entities')
 export class EntityController {
   constructor(private readonly entityService: EntityService) {}
 
   @Post()
-  create(@Body() createEntityDto: CreateEntityDto) {
-    return this.entityService.create(createEntityDto);
+  create(@Body() createEntityDto: CreateEntityDto , @GetUser('user_id') userId: number) {
+    return this.entityService.create(createEntityDto , userId);
   }
 
   @Get()
-  findAll() {
-    return this.entityService.findAll();
+  findAll( @GetUser('user_id') userId: number) {
+    return this.entityService.findAll(userId);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.entityService.findOne(+id);
+  findOne(@Param('id') id: string ,@GetUser('user_id') userId: number) {
+    return this.entityService.findOne(+id, userId);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEntityDto: UpdateEntityDto) {
-    return this.entityService.update(+id, updateEntityDto);
+  update(@Param('id') id: string, @Body() updateEntityDto: UpdateEntityDto, @GetUser('user_id') userId: number) {
+    return this.entityService.update(+id, updateEntityDto , userId);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.entityService.remove(+id);
+  remove(@Param('id') id: string, @GetUser('user_id') userId: number) {
+    return this.entityService.remove(+id , userId);
   }
 }
