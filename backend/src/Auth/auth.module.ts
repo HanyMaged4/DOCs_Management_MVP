c7737a6ac3d867f02a2e6d28546997ab877b9f3c
HanyMaged4 <hany.maged9102@gmail.com>
2025-10-27 19:16:17 +0300

    add utilities functions

--- backend/src/Auth/auth.module.ts
+++ backend/src/Auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthService } from "./auth.service";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { JwtStrategy } from "./strategy/jwt.strategy";
+import { EmailServiceService } from "./email-service.service";
 
 @Module({
     imports:[
@@ -13,7 +14,8 @@ import { JwtStrategy } from "./strategy/jwt.strategy";
     controllers: [AuthController],
     providers:[
         AuthService,
-        JwtStrategy
+        JwtStrategy,
+        EmailServiceService
     ]
 })
 export class AuthModule {}
