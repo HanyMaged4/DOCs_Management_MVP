55b39d8897dba4f5fe448fd82874072fa6501870
HanyMaged4 <hany.maged9102@gmail.com>
2025-10-29 11:40:26 +0300

    created the module

--- backend/src/cache/cache.module.ts
+++ backend/src/cache/cache.module.ts
@@ -1,7 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
+import { createClient } from 'redis';
 import { CacheService } from './cache.service';
+import { ConfigService } from '@nestjs/config';
 
+@Global()
 @Module({
-  providers: [CacheService]
-})
+    providers: [
+    {
+      provide: 'REDIS_CLIENT',
+      useFactory: async (config:ConfigService) => {
+        const url = config.get<string>('REDIS_URL') ?? 'redis://localhost:6379';
+        const client = createClient({ url});
+        await client.connect();
+        return client;
+        },
+    },
+    CacheService
+    ],
+    exports: ['REDIS_CLIENT', CacheService],
+  })
 export class CacheModule {}
--- backend/src/cache/cache.service.ts
+++ backend/src/cache/cache.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
-
+import { Inject, Injectable } from '@nestjs/common';
+import type { RedisClientType } from 'redis';
 @Injectable()
-export class CacheService {}
+export class CacheService {
+    constructor(
+        @Inject('REDIS_CLIENT') private readonly client:RedisClientType
+    ){}
+    
+    async get(key:string){
+        return this.client.get(key);
+    }
+    
+    async set( key:string , value :any , ttl:number){
+        if(ttl)
+            await this.client.setEx(key , ttl , JSON.stringify(value));
+        else
+            await this.client.set(key , JSON.stringify(value));
+    }
+
+    async del(key:string){
+        await this.client.del(key);
+    }
+}
