7e4b42dfb140ded54c87dde6454b99fc0efff1d4
HanyMaged4 <hany.maged9102@gmail.com>
2025-10-29 11:21:17 +0300

    switch to custum cashe

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,31 +8,11 @@ import { BookModule } from './book/book.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { EntityModule } from './entity/entity.module';
 import { AwsS3Module } from './aws-s3/aws-s3.module';
-import { CacheModule } from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-redis-store';
-import type { RedisClientOptions } from 'redis';
 import { MailModule } from './mail/mail.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-    }),
-    CacheModule.registerAsync<RedisClientOptions>({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      isGlobal: true, 
-      useFactory: async (config: ConfigService) => {
-        const host = config.get<string>('REDIS_HOST') ?? 'localhost';
-        const port = config.get<number>('REDIS_PORT') ?? 6379;
-        return {
-          store: redisStore as unknown as any,
-          socket: {
-            host,
-            port,
-          },
-          ttl: 600, 
-        };
-      },
     }),
     AuthModule, 
     UserModule, 
